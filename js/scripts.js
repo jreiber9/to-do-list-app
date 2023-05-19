de78a418100ef2090bae02966a7984fa1029fea5
jreiber9 <jreiber9@outlook.com>
2023-05-19 17:21:10 -0400

    finalized jquery changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 
 function newItem(){
 
-    //javascript
-    //1. Adding a new item to the list of items: 
        let li = $('<li></li>');
        let inputValue = $('#input').val();
        li.append(inputValue);
@@ -13,7 +11,6 @@ function newItem(){
          $('#list').append(li);
        }
     
-     //2. Crossing out an item from the list of items:
        function crossOut() {
              li.toggleClass('.strike');
          }
@@ -22,17 +19,16 @@ function newItem(){
             li.toggleClass('strike');
          });
     
-     //3(i). Adding the delete button "X": 
        let crossOutButton = $('<crossOutButton></crossOutButton>');
          crossOutButton.append(document.createTextNode('X'));
          li.append(crossOutButton);
     
          crossOutButton.on('click', deleteListItem);
-     //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+
        function deleteListItem(){
              li.addClass('delete')
          }
-     // 4. Reordering the items: 
+         
        $('#list').sortable();
     
     }
